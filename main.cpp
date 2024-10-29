7c1f9c7f90000398036d02a4aa3b21fcfc29d9da
Fira-Slavu Rares <raresdragutuu@gmail.com>
2024-10-29 17:41:18 +0200

    rezolvare stl 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
 using namespace std;
+
+struct spital {
+    string id, specializare;
+};
 
 int main()
 {
@@ -10,14 +14,15 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+    vector<spital>probleme, doctori;
+
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        probleme.push_back({name, speciality});
     }
 
     inFile >> no_doctors;
@@ -26,8 +31,18 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctori.push_back({name, speciality});
     }
 
+    for(auto i:probleme) {
+        int cnt = 0;
+        for(auto j:doctori)
+            if(i.specializare == j.specializare) {
+                cout << i.id << ' ' << "Acceptat\n";
+                cnt++;
+            }
+        if(!cnt)
+            cout << i.id << ' ' << "Respins\n";
+    }
     return 0;
 }
